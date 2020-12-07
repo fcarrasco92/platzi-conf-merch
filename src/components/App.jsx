f87cc678c4c2736281e1597a576a97dd049f3581
Felipe Carrasco <felipe@bakers.dev>
2020-12-07 01:02:13 -0300

    feature: configuration of eslint and prettier

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,10 @@
-import React from "react";
-import "../styles/components/app.css";
+import React from 'react';
+import '../styles/components/app.css';
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello world!</h1>
-    </div>
-  );
-};
+const App = () => (
+  <div>
+    <h1>Hello world!</h1>
+  </div>
+);
 
 export default App;
