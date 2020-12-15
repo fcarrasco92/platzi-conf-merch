bf5b315aa0767c1873fc2d16ae29544c8fb726a3
Felipe Carrasco <felipe@bakers.dev>
2020-12-15 07:43:40 -0300

    feature: style for payment and success

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import '../styles/components/Success.css';
 
 const Success = () => {
   return (
-    <div>
-      <h1>Success</h1>
+    <div className="Success">
+      <div className="Success-content">
+        <h2>Gracias por tu compra</h2>
+        <span>Tu pedido llegará en 3 dias a tu dirección</span>
+        <div className="Success-map">Google Maps</div>
+      </div>
     </div>
   );
 };
