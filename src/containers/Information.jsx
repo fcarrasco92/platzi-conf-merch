82e029ae44c0c9ff20fad093a629d6cd73aa73e6
Felipe Carrasco <felipe@bakers.dev>
2020-12-15 08:09:41 -0300

    integrate-icon-and-routes

--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/components/Information.css';
 
 const Information = () => {
@@ -23,7 +24,9 @@ const Information = () => {
         </div>
         <div className="Information-buttons">
           <div className="Information-back">Regresar</div>
-          <div className="Information-next">Pagar</div>
+          <Link to="/checkout/payment">
+            <div className="Information-next">Pagar</div>
+          </Link>
         </div>
       </div>
       <div className="Information-sidebar">
